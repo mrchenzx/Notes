f6c64e0c8485741eb7b822889ee5e422f5b6a8a6
chiupam <chiupam@126.com>
2021-06-21 21:43:00 +0800

    修正

--- JD/crontab.sh
+++ JD/crontab.sh
@@ -6,8 +6,8 @@ else
     dir_root=/ql
 fi
 dir_docker=$dir_root/scripts/docker
-file_name="crontab_liash.sh"
-url="https://ghproxy.com/https://raw.githubusercontent.com/chiupam/Notes/master/JD/crontab_liash.sh"
+file_name="crontab_list.sh"
+url="https://ghproxy.com/https://raw.githubusercontent.com/chiupam/Notes/master/JD/crontab_list.sh"
 cd $dir_root
 wget $url
 mv -f $file_name $dir_docker
